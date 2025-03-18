96ba5eada0c97e11f11e161f962d7e4db05fd4da
Rasmus Hans Christensen <rasmus.christensen@visma.com>
2025-03-18 17:51:57 +0100

    Update StupidService.cs

--- MinimapApi/MinimapApi/Services/StupidService.cs
+++ MinimapApi/MinimapApi/Services/StupidService.cs
@@ -32,32 +32,6 @@ public class StupidService : IStupidService
         return fullName + " is " + age + " years old6";
     }
     
-    public string GetStupid7()
-    {
-        var firstName = "carl";
-        var LastName = "jakobsen";
-        var fullName = firstName + LastName;
-        var yearBorn = 1990;
-        var currentYear = DateTime.Now.Year;
-        var age = currentYear - yearBorn;
-            
-        return fullName + " is " + age + " years old6";
-    }
-    
-    public string GetStupid10()
-    {
-        var firstName = "carl";
-        var firstName1 = "carl";
-        var firstName2 = "carl";
-        var firstName3 = "carl";
-        var LastName = "jakobsen";
-        var fullName = firstName + LastName;
-        var yearBorn = 1990;
-        var currentYear = DateTime.Now.Year;
-        var age = currentYear - yearBorn;
-            
-        return fullName + " is " + age + " years old6";
-    }
 
     public string GetStupid3()
     {
